4ba35ef12dfe049fba7717e4ef54f7ba7e801332
svtart <promixmaxads@gmail.com>
2022-10-07 20:35:31 +0300

    Class "Cat" added

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,31 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        Cat bean2 =
+               applicationContext.getBean("cat", Cat.class);
+        System.out.println(bean2.getMessage());
+
+        HelloWorld bean3 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat bean4 =
+                applicationContext.getBean("cat", Cat.class);
+        System.out.println(bean2.getMessage());
+
+        Cat bean5 =
+                applicationContext.getBean("cat", Cat.class);
+        System.out.println(bean2.getMessage());
+
+        System.out.println(bean.equals(bean3));
+        System.out.println(bean2.equals(bean4));
+
+
+
     }
 }
