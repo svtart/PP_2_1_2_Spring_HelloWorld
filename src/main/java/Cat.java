584c1da9b3c3a1be1ac979535e62537ef7e4d425
svtart <promixmaxads@gmail.com>
2022-10-10 17:48:12 +0300

    Class "Cat" added

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+import org.springframework.stereotype.Component;
+
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
